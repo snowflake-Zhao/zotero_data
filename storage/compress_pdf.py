9a8e0963a6a57e4543de08323c3d3302907fa8ff
snowflake-Zhao <763585773@qq.com>
2025-06-20 17:23:34 +0800

    update the compress script update the pdf as well

--- storage/compress_pdf.py
+++ storage/compress_pdf.py
@@ -10,12 +10,15 @@ def compress_n_update(pdf_file_dir,pdf_filename):
     for page in writer.pages:
         page.compress_content_streams()  # This is CPU intensive!
 
-
     # Split filename and extension
     filename, extension = os.path.splitext(pdf_filename)
+    compress_suffix = "_compress"
+    if(filename.endswith(compress_suffix)):
+        print(f"The {pdf_filename} is already compressed")
+        return
 
     # Create new filename
-    new_filename = f"{filename}_compress{extension}"
+    new_filename = f"{filename}{compress_suffix}{extension}"
     new_filepath = os.path.join(pdf_file_dir, new_filename)
 
     print(f"compress_n_update filename: {pdf_filename}")
@@ -25,14 +28,16 @@ def compress_n_update(pdf_file_dir,pdf_filename):
     old_filesize = os.path.getsize(pdf_filepath)
     new_filesize = os.path.getsize(new_filepath)
     print(f"the file size of filename: {pdf_filename} is {old_filesize}")
-    print(f"the file size of filename: {new_filename} is {new_filesize}")
+    print(f"the new file size of filename: {new_filename} is {new_filesize}")
     if old_filesize <= new_filesize:
         os.remove(new_filepath)
         print(f"remove the file {pdf_filename} successfully")
         return
     os.remove(pdf_filepath)
-    os.rename(new_filename,pdf_filename)
+    os.rename(new_filepath,pdf_filepath)
     print(f"Renamed {pdf_filename} to {new_filename}")
 if __name__ == "__main__":
-    current_dir = "./2AT7UNZU"
-    compress_n_update("./2AT7UNZU",[f for f in os.listdir(current_dir) if f.lower().endswith('.pdf')][0])
+    for dir in os.listdir('.'):
+        if len([f for f in os.listdir(dir) if f.lower().endswith('.pdf')]) <= 0:
+            continue
+        compress_n_update(dir,[f for f in os.listdir(dir) if f.lower().endswith('.pdf')][0])
